38ffb604f43b8876cc7d141913f3bae85bb52ef7
Ahumada Luis Braian <lucho.25405980@gmail.com>
2026-05-30 03:05:56 -0300

    se aplico estilo y se creo la nueva pagina 'destacado'

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const meriendas = ["Torta de chocolate", "Galletitas de avena", "Muffins de arándanos",
-    "Panqueques con miel", "Sándwich de jamón y queso"];
+const meriendas = ["Torta de chocolate.", "Galletitas de avena.", "Muffins de arándanos.",
+    "Panqueques con miel.", "Sándwich de jamón y queso."];
 
 const listasMeriendas=document.getElementById("listasMeriendas");
 const botonDeCantidad=document.getElementById("botonDeCantidad");
 const cantidad=document.getElementById("cantidad");
 
 meriendas.forEach((merienda) => {
-    listasMeriendas.innerHTML += "<div>" + merienda + "</div>";
+    listasMeriendas.innerHTML += "<li  class='Meriendas'>" + merienda + "</li>";
 });
 
 
