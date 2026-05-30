c94985982cf43043ef1f34d8c923369f154af61b
Ahumada Luis Braian <lucho.25405980@gmail.com>
2026-05-30 13:00:47 -0300

    Logica implementada a la pagina de 'listado'

---
+++ listado.js
@@ -0,0 +1,31 @@
+const juegos = [
+    { nombre: "Minecraft",categoria: "supervivencia"},
+    { nombre: "Fortnite",categoria: "battle royale"},
+    { nombre: "ARK", categoria: "supervivencia"},
+    { nombre: "GTA V", categoria: "accion-aventura"},
+    { nombre: "The Forest", categoria: "supervivencia"},
+    { nombre: "LOL", categoria: "MOBA"},
+]
+
+const listado = document.getElementById("lista");
+const botonTodos = document.getElementById("botonTodos");
+const botonSupervivencia = document.getElementById("botonSupervivencia");
+
+function mostrarJuegos(juegos) {
+    listado.innerHTML = "";
+
+    juegos.forEach((juego) => {
+        listado.innerHTML += `<li class='juego'>${juego.nombre} - ${juego.categoria}</li>`;
+    });
+}
+
+mostrarJuegos(juegos);
+
+botonTodos.addEventListener("click", () => {
+    mostrarJuegos(juegos);
+});
+
+botonSupervivencia.addEventListener("click", () => {
+    const juegosSupervivencia = juegos.filter(juego => juego.categoria === "supervivencia");
+    mostrarJuegos(juegosSupervivencia);
+});
